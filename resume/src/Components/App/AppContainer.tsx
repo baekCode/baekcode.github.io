40d279073cd1d167ede97b8457316f88b1040848
baekCode <sosaer88@gmail.com>
2020-08-19 21:20:46 +0900

    feat : 기본 라우트 정의

---
+++ resume/src/Components/App/AppContainer.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import AppPresenter from './AppPresenter';
+
+const isInput = false;
+
+const AppContainer = () => <AppPresenter isInput={isInput} />;
+
+export default AppContainer;
