6621d6d0f6bd39ea3fd38dbed2ca866c5ab4cc45
baekcCode <baekcode@gmail.com>
2021-11-10 11:45:03 +0900

    feat : simple sort & eslint prettier semi

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,11 +1,11 @@
-import tw from 'twin.macro'
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
+import tw from 'twin.macro';
 
 const TextStyle = styled.div`
   ${tw`text-sm`}
-`
+`;
 
 export default function Text({ text }: { text: string }) {
-  return <TextStyle>{text}</TextStyle>
+  return <TextStyle>{text}</TextStyle>;
 }
