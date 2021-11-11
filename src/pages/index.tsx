2822385f2c9a3357570c72c84874306ec75c5155
baekcCode <baekcode@gmail.com>
2021-11-12 00:49:28 +0900

    feat : posts 적용

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,26 +2,23 @@ import { graphql } from 'gatsby';
 import React from 'react';
 
 import { CATEGORY } from '../_mock/category';
+import { POSTS } from '../_mock/posts';
 import Category from '../components/category';
 import Layout from '../components/layout';
-import Text from '../components/Text';
+import Posts from '../components/posts';
 import { IIndexProps } from './types';
 
 export default function Index({
   data: {
     site: {
-      siteMetadata: { title, description, author },
+      siteMetadata: { title },
     },
   },
 }: IIndexProps) {
   return (
     <Layout title={title}>
       <Category selectedCategory="Web" categoryList={CATEGORY} />
-      <div>
-        <Text text={title} />
-        <Text text={description} />
-        <Text text={author} />
-      </div>
+      <Posts posts={POSTS} />
     </Layout>
   );
 }
