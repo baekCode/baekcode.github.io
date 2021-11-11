8712dede1f2a767e8f07a74d462fd16df0d57475
baekcCode <baekcode@gmail.com>
2021-11-12 00:48:48 +0900

    feat : posts mock data

---
+++ src/_mock/posts.ts
@@ -0,0 +1,22 @@
+export const POSTS = [
+  {
+    title: 'Post Item Title1',
+    date: '2020.01.29.',
+    categories: ['Web', 'Frontend', 'Testing'],
+    summary:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident repellat doloremque fugit quis rem temporibus! Maxime molestias, suntrem debitis odit harum impedit. Modi cupiditate harum dignissimos eos in corrupti!',
+    thumbnail:
+      'https://images.unsplash.com/photo-1559715541-5daf8a0296d0?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cnViYmVyJTIwZHVja3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80',
+    link: 'https://www.google.co.kr/',
+  },
+  {
+    title: 'Post Item Title2',
+    date: '2020.01.29.',
+    categories: ['Web', 'Frontend', 'Testing'],
+    summary:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident repellat doloremque fugit quis rem temporibus! Maxime molestias, suntrem debitis odit harum impedit. Modi cupiditate harum dignissimos eos in corrupti!',
+    thumbnail:
+      'https://images.unsplash.com/photo-1559715541-5daf8a0296d0?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cnViYmVyJTIwZHVja3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80',
+    link: 'https://www.google.co.kr/',
+  },
+];
