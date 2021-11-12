976cdb5bc29219bcccb0d9046929be866a60f479
baekcCode <baekcode@gmail.com>
2021-11-12 15:01:49 +0900

    feat : post template 임시 구현

---
+++ src/templates/postTemplate.tsx
@@ -0,0 +1,30 @@
+import { graphql } from 'gatsby';
+import React from 'react';
+
+export default function PostTemplate(props) {
+  console.log(props);
+  return <div>PostTemplate</div>;
+}
+
+export const queryMarkdownDataBySlug = graphql`
+  query queryMarkdownDataBySlug($slug: String) {
+    allMarkdownRemark(filter: { fields: { slug: { eq: $slug } } }) {
+      edges {
+        node {
+          html
+          frontmatter {
+            title
+            summary
+            date(formatString: "YYYY.MM.DD.")
+            categories
+            thumbnail {
+              childImageSharp {
+                gatsbyImageData
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
