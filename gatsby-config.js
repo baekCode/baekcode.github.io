b7331ebd017cbc7dc4943b64cd807516817c111d
baekcCode <baekcode@gmail.com>
2021-11-13 14:34:50 +0900

    chore : site meta

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `baekcode Front-end & UI Developer`,
+    description: `Front-end & UI Developer Blog`,
+    author: `@baekcode`,
+    siteUrl: `https://baekcode.github.io/`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
