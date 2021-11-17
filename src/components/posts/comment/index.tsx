ffd3dbb8af9421ab0144bb7890d8386bed2081e1
baekcCode <baekcode@gmail.com>
2021-11-17 14:10:19 +0900

    fix : deploy시에 env 참고할수 없는 버그 수정
    - 배포 후 댓글 이 연동이 안되는 버그
    - 원인 : 배포 후 env 참고하지 못함, 값이 undefined
    - 해결 방법 : 추후 다른곳에서도 배포 할시 env 포함해서 하던지 또는 constant 참고 하는 방식으로 수정

--- src/components/posts/comment/index.tsx
+++ src/components/posts/comment/index.tsx
@@ -1,6 +1,7 @@
 import React, { createRef, useEffect } from 'react';
 
 import { UtterancesAttributesType } from './types';
+import { GITHUB_REPOSITORY, UTTERANCES_SRC } from '../../../constants';
 
 export default function Comment() {
   const element = createRef<HTMLDivElement>();
@@ -11,8 +12,8 @@ export default function Comment() {
     const utterances: HTMLScriptElement = document.createElement('script');
 
     const attributes: UtterancesAttributesType = {
-      src: process.env.UTTERANC_SRC as string,
-      repo: process.env.GITHUB_REPOSITORY as string,
+      src: process.env.UTTERANC_SRC as string || UTTERANCES_SRC,
+      repo: process.env.GITHUB_REPOSITORY as string || GITHUB_REPOSITORY,
       'issue-term': 'title',
       label: 'Comment',
       theme: `github-light`,
---
+++ src/constants/index.ts
@@ -0,0 +1,2 @@
+export const UTTERANCES_SRC = 'https://utteranc.es/client.js';
+export const GITHUB_REPOSITORY = 'baekCode/baekCode.github.io';
