1ff87420803288d5bc9bec1ef4c6a2a58092c2a8
77unny <baekcode@gmail.com>
2023-01-11 07:37:08 +0900

    feat: index getStaticProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,20 @@
 import { IndexPage } from '~apps/index/IndexPage';
 import { getDatabase } from '~apps/libs/notion';
 
+/**
+ * @description getStaticProps 이유?
+ * 굳이 계속 반복 호출을 할 필요가 있나?
+ * revalidate 로 캐싱 및 호출 시간을 제어 한다.
+ * */
 export const getStaticProps = async () => {
   const { posts, title } = await getDatabase();
 
   return {
     props: {
-      posts: posts,
+      posts,
       title,
     },
-    revalidate: 1,
+    revalidate: 10,
   };
 };
 
